7731e954e2f3a7c5d48cd7031d27cfd5ab5689f5
Leonardo Madeira <leomadeiraa@hotmail.com>
2021-11-06 21:34:07 -0300

    commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { Dashboard } from "./Dashboard";
+import { Header } from "./Header";
+import Modal from 'react-modal'
+import { GlobalStyle } from "./styles/global";
+import { NewTrasactionModal } from "./NewTransactionModal";
+
+Modal.setAppElement('#root');
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false); //ele ja começa como false pq ainda nao foi clicado
+
+  function handleOpenNewTransactionModal(){ //onClick para quando o cliente clicar no botão, assim ele será true(verdadeiro)
+      setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal(){ //Fecha se clicar esc, clicar na tela preta, x e etc...
+      setIsNewTransactionModalOpen(false)
+  }
+  return (
+    <>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+      <NewTrasactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
+    </>
+  );
+}
---
+++ src/Summary/index.tsx
@@ -0,0 +1,38 @@
+import incomeImg from '../assets/entrada.svg'
+import imgSaida from '../assets/saídas.svg'
+import imgTotal from '../assets/total.svg'
+import { Container } from "./styles";
+
+export function Summary(){
+    return(
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Entradas" />
+                </header>
+                <strong>
+                    R$1000,00
+                </strong>
+            </div>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={imgSaida} alt="Saida" />
+                </header>
+                <strong>
+                    R$-500,00
+                </strong>
+            </div>
+            <div className="green-total">
+                <header>
+                    <p>Entradas</p>
+                    <img src={imgTotal} alt="Total" />
+                </header>
+                <strong>
+                    R$500,00
+                </strong>
+            </div>
+        </Container>
+    )
+}
